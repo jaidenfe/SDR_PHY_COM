23b509503fcb696a20cdfbe2488935171a1da491
Jaide Ferraccioli <jaidenfe@buffalo.edu>
2016-06-14 13:27:52 -0400

    Updated Code

--- gr-Interfaces/build/apps/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/ubnl-sof/COM/SDR_PHY_COM/gr-Interfaces/apps
-# Build directory: /home/ubnl-sof/COM/SDR_PHY_COM/gr-Interfaces/build/apps
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
